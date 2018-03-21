75ede08fa67cc2a9186b02e1587b4e80b7ac3863
Graziantonio <derobertis.graziantonio@gmail.com>
2018-03-21 10:49:39 +0100

    Added player selection for family background.

--- files/character_creation.py
+++ files/character_creation.py
@@ -53,7 +53,7 @@ print('\n')
 ccv.lifepath['name'] = input('Name your character: ').title()
 
 while True:
-    gender_selection = input(f"Is {ccv.lifepath['name']} Male or Female?").title()
+    gender_selection = input(f"Is {ccv.lifepath['name']} Male or Female? ").title()
     gender_selection.upper()
     if gender_selection == 'M' or gender_selection == 'MALE' or gender_selection == 'F' or gender_selection == 'FEMALE':
         ccv.lifepath['gender'] = gender_selection
@@ -62,7 +62,7 @@ while True:
         print('Please enter [M]ale or [F]emale.')
 
 while True:
-    age_selection = int(input(f"How old is {ccv.lifepath['name']}?"))
+    age_selection = int(input(f"How old is {ccv.lifepath['name']}? "))
     if age_selection > 16:
         ccv.lifepath['age'] = age_selection
         break
@@ -87,6 +87,7 @@ else:
     player_selection(ccv.clothes, ccv.lifepath, 'clothes', '### CLOTHING ###')
     player_selection(ccv.hairstyle, ccv.lifepath, 'hairstyle', '### HAIRSTYLE ###')
     player_selection(ccv.accessories, ccv.lifepath, 'accessories', '### ACCESSORIES ###')
+    print('\n')
 
 # Family background setup.
 
@@ -104,6 +105,9 @@ if randomized:
     if ccv.lifepath['parents'] == 'something happened':
         selection = dice.roll(1, 10)
         ccv.lifepath['something_happened'] = ccv.something_happened[selection - 1]
+    else:
+        # ccv.lifepath['parents'] is already 'both living'
+        ccv.lifepath['something_happened'] = 'N/A'
 
     # current family status
     selection = dice.roll(1, 2)
@@ -113,6 +117,9 @@ if randomized:
     if ccv.lifepath['family_status'] == 'in danger':
         selection = dice.roll(1, 10)
         ccv.lifepath['family_tragedy'] = ccv.family_tragedy[selection - 1]
+    else:
+        # ccv.lifepath['family_status'] is already 'ok, despite everything'
+        ccv.lifepath['family_tragedy'] = 'N/A'
 
     # childhood
     selection = dice.roll(1, 10)
@@ -122,16 +129,52 @@ if randomized:
     selection = dice.roll(1, 5)
     ccv.lifepath['siblings'] = ccv.siblings[selection - 1]
 
+else:
+
+    # family ranking
+    player_selection(ccv.family_ranking, ccv.lifepath, 'family_ranking', '### FAMILY RANKING ###')
+
+    # parents
+    player_selection(ccv.parents, ccv.lifepath, 'parents', '### PARENTS ###')
+
+    # if something happened to parents, set what happened
+    if ccv.lifepath['parents'] == 'something happened':
+        player_selection(ccv.something_happened, ccv.lifepath, 'something_happened', '### SOMETHING HAPPENED ###')
+    else:
+        # ccv.lifepath['parents'] is already 'both living'
+        ccv.lifepath['something_happened'] = 'N/A'
+
+    # current family status
+    player_selection(ccv.family_status, ccv.lifepath, 'family_status', '### CURRENT FAMILY STATUS ###')
+
+    # if family in danger, set what's happening
+    if ccv.lifepath['family_status'] == 'in danger':
+        player_selection(ccv.family_tragedy, ccv.lifepath, 'family_tragedy', "### WHAT'S HAPPENING ###")
+    else:
+        # ccv.lifepath['family_status'] is already 'ok, despite everything'
+        ccv.lifepath['family_tragedy'] = 'N/A'
+
+    # childhood
+    player_selection(ccv.childhood, ccv.lifepath, 'childhood', '### CHILDHOOD ENVIRONMENT ###')
+
+    # siblings
+    player_selection(ccv.siblings, ccv.lifepath, 'siblings', '### SIBLINGS ###')
+
 
 def main():
 
+    # print('\n')
+    # print(f"Wanted: {ccv.lifepath['name']}")
+    # print(f"Sex: {ccv.lifepath['gender']}")
+    # print(f"Age: about {ccv.lifepath['age']}")
+    # print('\n')
+    # print('General description:')
+    # print(f"Wears {ccv.lifepath['clothes']} and {ccv.lifepath['accessories']}.")
+    # print(f"Has {ccv.lifepath['hairstyle']} hair.")
+
+    # TEST (check if everything is stored correctly)
     print('\n')
-    print(f"Wanted: {ccv.lifepath['name']}")
-    print(f"Sex: {ccv.lifepath['gender']}")
-    print(f"Age: about {ccv.lifepath['age']}")
-    print('General description:')
-    print(f"Wears {ccv.lifepath['clothes']} and {ccv.lifepath['accessories']}.")
-    print(f"Has {ccv.lifepath['hairstyle']} hair.")
+    print(ccv.lifepath)
 
 
 if __name__ == '__main__':
